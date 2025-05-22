1bd02a98ca7264589b11d55015500880cacf84e4
patrick <habiyaremyepatrick.hp@gmail.com>
2025-05-22 20:00:05 +0200

    props

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Student from './Student.jsx'
+
+function App() {
+  return(
+    <>
+    <Student name="Bello" age={20} isStudent={false}/>
+    <Student name="Patrick" age={23} isStudent={true}/>
+
+    </>
+  );
+}
+
+export default App
